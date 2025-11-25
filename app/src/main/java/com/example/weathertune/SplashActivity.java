1080884a82012d70fda6fe6bac5980348f5e28ad
BaeSuha <shbae9160@gmail.com>
2025-11-26 04:04:21 +0900

    feat: 초기메인화면 ui구현

---
+++ app/src/main/java/com/example/weathertune/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.weathertune;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 3000; // 3초
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 3초 후 MainActivity로 이동
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
